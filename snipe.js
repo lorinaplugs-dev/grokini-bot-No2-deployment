394347f92870d49993c3c8c122aa42ae6514ae6a
lorinaplugs-dev <223964821+lorinaplugs-dev@users.noreply.github.com>
2026-02-22 13:44:24 +0100

    Update snipe.js

--- snipe.js
+++ snipe.js
@@ -852,7 +852,7 @@ async function showMainMenu(ctx, edit = false) {
     : '⚠️ No wallet connected';
   
   const message = `
-🚀 *Hey Welcome to Maestro Sniper Bot* 🤖
+🚀 *Hey Welcome to Sol Trading Bot- New* 🤖
 
 
 I’m the one-stop solution for all your trading needs!
