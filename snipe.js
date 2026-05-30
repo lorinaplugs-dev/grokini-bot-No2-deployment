9f238e345cbcbef41408e41dd1d5cce03b5e6c92
lorinaplugs-dev <223964821+lorinaplugs-dev@users.noreply.github.com>
2026-05-30 23:45:12 +0100

    Update welcome message for Sniper Bot

--- snipe.js
+++ snipe.js
@@ -1409,7 +1409,7 @@ async function showMainMenu(ctx, edit = false) {
     }
     
     const message = `
-🚀 *Welcome Maestro Sniper Bot* 🤖
+🚀 *Welcome Solbix Sniper Bot* 🤖
 
 *I'm your Web3 execution engine*.
 ━━━━━━━━━━━━━━━━━━
